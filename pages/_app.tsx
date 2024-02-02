a6735147d3b74e1cdee58b36a0a592b8bd5b2d23
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-02-02 13:54:14 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,12 @@ import Navbar from "../components/Navbar";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "p0lygon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}
+    <ThirdwebProvider  
+       activeChain="polygon"
        clientId="7976449a6362f89d61ead11d0753cfd8"
       >
       <Navbar />
